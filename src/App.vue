d1206149318787e278eb1a98e1e7b9279b901dcf
Ravi-Rathod-22 <raviarthod4449@gmail.com>
2022-10-13 23:53:30 +0530

    Add Learning Resource Component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
     <ul>
-        
+        <LearningResource v-for="res in storedResources" :key="res.id" :title="res.title" :description="res.description" :link="res.link"></LearningResource>
     </ul>
 </template>
 
 
 <script>
-
+import LearningResource from "./components/learning-resource/LearningResource.vue";
 export default {
+    components:{
+        LearningResource
+    },
     data(){
         return{
             storedResources: [
