7a76c5f7e8c01020d77216f35705f988ef72afc3
Ravi-Rathod-22 <raviarthod4449@gmail.com>
2022-12-10 19:16:37 +0530

    Add base card and header components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
+  <the-header title="Remember Me"></the-header>
   <stored-resource :resources="storedResources"></stored-resource>
 </template>
 
 <script>
 import StoredResource from './components/learning-resource/StoredResource.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
 export default {
   components: {
     StoredResource,
+    TheHeader,
   },
   data() {
     return {
