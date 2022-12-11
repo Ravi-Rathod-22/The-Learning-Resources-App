b8ef1f2ee7e4e96aa0e1919c4a6f9b1f33621a67
Ravi-Rathod-22 <raviarthod4449@gmail.com>
2022-12-11 19:42:35 +0530

    Add dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <the-header title="Remember Me"></the-header>
-  <stored-resource :resources="storedResources"></stored-resource>
 </template>
 
 <script>
-import StoredResource from './components/learning-resource/StoredResource.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
-    StoredResource,
     TheHeader,
   },
   data() {
---
+++ src/components/learning-resource/TheResource.vue
@@ -0,0 +1,33 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resorces')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resorce')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resorces',
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
